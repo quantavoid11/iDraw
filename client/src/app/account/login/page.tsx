3b52bae4d38066e5c96337125b6e21a991d0f3a1
Siddhant Kumar Keshri <siddhantkeshri2806@gmail.com>
2023-10-07 22:29:52 +0530

    account routes added

---
+++ client/src/app/account/login/page.tsx
@@ -0,0 +1,6 @@
+import type { NextPage } from "next";
+
+const LoginPage:NextPage=()=>{
+    return (<h1>hello from login page</h1>);
+}
+export default LoginPage;
---
+++ client/src/app/account/signup/page.tsx
@@ -0,0 +1,6 @@
+import type { NextPage } from "next";
+
+const signUpPage:NextPage=()=>{
+    return (<h1>hello from signup page</h1>);
+}
+export default signUpPage;
